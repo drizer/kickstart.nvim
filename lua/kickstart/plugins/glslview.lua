780634b9184d60cc777d8cc21b394b7b39c5cf2e
Derek Mall <derek.mall@outlook.com>
2024-07-14 14:05:22 +1000

    Add glslview plugin

---
+++ lua/kickstart/plugins/glslview.lua
@@ -0,0 +1,14 @@
+-- glslView nvim
+
+return {
+  -- glslView nvim
+  'timtro/glslView-nvim',
+  ft = { 'glsl', 'frag', 'vert' },
+  --config = function(_)
+
+  -- require('glslView').setup {
+  --  viewer_path = 'glslViewer',
+  --  args = { '-l' },
+  --}
+  --end,
+}
